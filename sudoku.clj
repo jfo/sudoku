b97a4958ee3815f50a4dea9d8cc6fe79ec1bfaf3
urthbound <jeffowler@gmail.com>
2014-04-08 03:19:05 -0400

    cleanup

--- sudoku.clj
+++ sudoku.clj
@@ -1,16 +1,21 @@
 (defn coords []
+  "returns all possible coordinates in order"
   (for [x (range 9) y (range 9)] (vector x y)))
-(defn empty-map [] (into {} (for [x (coords)] {x 0})))
 
-(def tester (empty-map))
+(defn empty-map []
+  "empty sudoku board for testing naive solution"
+  (into {} (for [x (coords)] {x 0})))
 
 (defn return-column [x puzzle]
+  "given an x value, returns a set of coords in that column"
   (set (filter #(= (first %) x) (keys puzzle))))
 (defn return-row [x puzzle]
+  "given a y value, returns a set of coords in that row"
   (set (filter #(= (last %) x) (keys puzzle))))
 
 
 (defn return-square-set [n]
+  "given a number, returns a set to which it belongs"
   (let [i (quot n 3)]
     (case i
       0 #{0 1 2}
@@ -22,11 +27,13 @@
 (defn return-y-square [coords puzzle]
   (set (filter #(contains? (return-square-set (last coords)) (last %)) (keys puzzle))))
 (defn return-square [coords]
+  "intersects x and y sets to find square"
   (clojure.set/intersection
     (set (return-x-square coords (empty-map)))
     (set (return-y-square coords (empty-map)))))
 
 (defn show-friends [coords puzzle]
+  "returns a set of all squares that need to be checked against for a given coord"
   (vec (set
   (apply concat
   [(return-column (first coords) (empty-map))
@@ -37,33 +44,24 @@
 (show-friends [0 0] tester)
 
 (defn show-friend-set [co puzzle]
+  "returns a set of used values in friends"
   (set (vals (select-keys puzzle (show-friends co puzzle)))))
 
-(show-friend-set [0 0], (assoc (empty-map) [0,0] 1))
-
 (defn find-winner [co puzzle]
+  "iteratively discovers first available number against friend set for a given coord and puzzle"
   (loop [friend-set (show-friend-set co puzzle)
         n 1]
       (if (not (contains? friend-set n))
         (assoc puzzle co n)
         (recur friend-set (+ n 1)))))
 
-(defn printer [puzzle]
-  (vals (sort-by key puzzle)))
-(printer tester)
 
 (defn solver [puzzle]
+  "recursively finds a winner for each cell. No checking against max val or tree traversal yet"
     (let [co (some #(if (= 0 (puzzle %)) % ) (coords))]
       (if (= nil co)
         puzzle
         (solver (find-winner co puzzle)))))
 
-(last (coords))
-
-(printer (solver tester))
-(tester [0 0])
-(pprint (solver2 tester))
-
-(vals tester)
-(find-winner [1 0] (assoc (empty-map) [0, 0] 0) )
-
+(solver tester)
+(vals (sort-by key (solver tester)))
