6fef2763685f0af3d08889e767cb8888e2675eb6
urthbound <jeffowler@gmail.com>
2014-04-08 10:30:06 -0400

    formatting

--- sudoku.clj
+++ sudoku.clj
@@ -1,17 +1,25 @@
 (defn coords []
   "returns all possible coordinates in order"
-  (for [x (range 9) y (range 9)] (vector x y)))
+  (for [x (range 9) y (range 9)]
+    (vector x y)))
 
 (defn empty-map []
   "empty sudoku board for testing naive solution"
-  (into {} (for [x (coords)] {x 0})))
+  (into {}
+        (for [x (coords)]
+          {x 0})))
 
 (defn return-column [x puzzle]
   "given an x value, returns a set of coords in that column"
-  (set (filter #(= (first %) x) (keys puzzle))))
+  (set (filter
+         #(= (first %) x)
+         (keys puzzle))))
+
 (defn return-row [x puzzle]
   "given a y value, returns a set of coords in that row"
-  (set (filter #(= (last %) x) (keys puzzle))))
+  (set (filter
+         #(= (last %) x)
+         (keys puzzle))))
 
 
 (defn return-square-set [n]
@@ -23,9 +31,19 @@
       2 #{6 7 8})))
 
 (defn return-x-square [coords puzzle]
-  (set (filter #(contains? (return-square-set (first coords)) (first %)) (keys puzzle))))
+  (set (filter
+         #(contains?
+            (return-square-set (first coords))
+            (first %))
+         (keys puzzle))))
+
 (defn return-y-square [coords puzzle]
-  (set (filter #(contains? (return-square-set (last coords)) (last %)) (keys puzzle))))
+  (set (filter
+         #(contains?
+            (return-square-set (last coords))
+            (last %))
+         (keys puzzle))))
+
 (defn return-square [coords]
   "intersects x and y sets to find square"
   (clojure.set/intersection
@@ -33,19 +51,19 @@
     (set (return-y-square coords (empty-map)))))
 
 (defn show-friends [coords puzzle]
-  "returns a set of all squares that need to be checked against for a given coord"
-  (vec (set
-  (apply concat
-  [(return-column (first coords) (empty-map))
-  (return-row (last coords) (empty-map))
-  (return-square coords)]))))
+  "returns a set of all squares that need to be checked against for a given coord ('friends')"
+  (vec (set (apply concat [(return-column (first coords) (empty-map))
+                           (return-row (last coords) (empty-map))
+                           (return-square coords)]))))
 
 (show-friend-set [0 0] (assoc tester [0 0] 8))
 (show-friends [0 0] tester)
 
 (defn show-friend-set [co puzzle]
   "returns a set of used values in friends"
-  (set (vals (select-keys puzzle (show-friends co puzzle)))))
+  (set (vals (select-keys
+               puzzle
+               (show-friends co puzzle)))))
 
 (defn find-winner [co puzzle]
   "iteratively discovers first available number against friend set for a given coord and puzzle"
@@ -65,3 +83,4 @@
 
 (solver tester)
 (vals (sort-by key (solver tester)))
+
