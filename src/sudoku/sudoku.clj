31758d36feb46d1273cbe2fb9ead978fbec3912f
urthbound <jeffowler@gmail.com>
2014-04-15 13:09:47 -0400

    zachs first refactor

--- src/sudoku/sudoku.clj
+++ src/sudoku/sudoku.clj
@@ -42,7 +42,7 @@
     (set (return-y-square coords (empty-map)))))
 
 (defn show-friends [coords]
-  "returns a set of all squares that need to be checked against for a given coord ('friends')"
+  "returns a set of all cells that need to be checked against for a given coord ('friends')"
   (vec (set (apply concat [(return-column (first coords) (empty-map))
                            (return-row (last coords) (empty-map))
                            (return-square coords)]))))
@@ -87,54 +87,99 @@
           {}
           puzzle))
 
+; =======================================================================
+
+
+(defn all-possible-moves [puzzle]
+  (mapcat (fn [pair]
+            (let [[coords moves] pair]
+              (for [move moves]
+                [coords move])))
+          (possibles puzzle)))
+
+(defn make-move [puzzle move]
+  (assoc puzzle (first move) (last move)))
+
+(defn solved? [puzzle]
+  (every? (complement zero?) (vals puzzle)))
+
+
+(declare solve)
+
+(defn try-moves [puzzle moves]
+    (when (empty? moves)
+          (throw (Exception. "no valid moves")))
+    (let [next-puzzle (make-move puzzle (first moves))]
+          (try
+                  (solve next-puzzle)
+                  (catch Exception e
+                            (try-moves puzzle (rest moves))))))
+
+(defn solve [puzzle]
+    (if (solved? puzzle)
+          puzzle
+          (try-moves puzzle (all-possible-moves puzzle))))
+
+(solve puzzle)
+
+
+
+(all-possible-moves puzzle)
+
+(sort (possibles puzzle))
+(do
+(print-puzzle puzzle)
+(println)
+(print-puzzle (assoc puzzle (first (guess puzzle))(last (guess puzzle)))))
+
+
 
 ; everything below this line sucks and doesn't work and will never work.
 ; =======================================================================
 
-(defn guess
-  ([puzzle] (guess puzzle {}))
-  ([puzzle tried]
-   (let [g (reduce (fn [acc cell]
-                     (assoc acc
-                            (key cell)
-                            (clojure.set/difference
-                              (val cell)
-                              (tried (key cell)))))
-                   {}
-                   (possibles puzzle))]
-     (let [guess (sort-by #(count (val %)) g)]
-     [(first (first guess)) (first (last (first guess)))]   ))))
-
-
-(defn rec-solve
-  ([puzzle] (rec-solve puzzle {}))
-  ([puzzle tried]
-   (let [board (solve-all puzzle)]
-     (if (not (contains? (set (vals board)) 0))
-       board
-       (if (contains? (set (vals (possibles board))) #{})
-         (throw (Exception. "Ughh"))
-         (try
-           (rec-solve
-             (assoc board
-                    (first (guess board tried))
-                    (last (guess board tried)))
-             (assoc tried
-                    (first (guess board tried))
-                    (clojure.set/union
-                      #{(last (guess board tried))}
-                       ((possibles board) (first (guess board tried))))))))))))
+; (defn guess
+;   ([puzzle] (guess puzzle {}))
+;   ([puzzle tried]
+;    (let [g (reduce (fn [acc cell]
+;                      (assoc acc
+;                             (key cell)
+;                             (clojure.set/difference
+;                               (val cell)
+;                               (tried (key cell)))))
+;                    {}
+;                    (possibles puzzle))]
+;      (let [guess (sort-by #(count (val %)) g)]
+;      [(first (first guess)) (first (last (first guess)))]   ))))
+; (defn rec-solve
+;   ([puzzle] (rec-solve puzzle {}))
+;   ([puzzle tried]
+;    (let [board (solve-all puzzle)]
+;      (if (not (contains? (set (vals board)) 0))
+;        board
+;        (if (or (contains? (set (vals (possibles board))) #{}) (contains? (set (vals board)) nil))
+;          (throw (Exception. "Ughh"))
+;          (try
+;            (rec-solve
+;              (assoc board
+;                     (first (guess board tried))
+;                     (last (guess board tried)))
+;              (assoc tried
+;                     (first (guess board tried))
+;                     (clojure.set/union
+;                       #{(last (guess board tried))}
+;                        ((possibles board) (first (guess board tried))))))))))))
 
 ; demo stuff
 
-; (def puzzle (gen-puzzle))
-; (do 
-; (print-puzzle puzzle)
-;   (println)
-; (print-puzzle (solve-all puzzle))
-;   )
+(def puzzle (gen-puzzle))
+(do
+(print-puzzle puzzle)
+  (println)
+(print-puzzle (solve-all puzzle))
+  (println)
+(print-puzzle (rec-solve puzzle))
+  )
 
 ; (possibles (solve-all puzzle))
-; (guess puzzle)
+(guess puzzle {[3 2] #{3}})
 
-; (print-puzzle (rec-solve puzzle))
